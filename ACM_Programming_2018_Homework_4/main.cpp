92abc5c2f94e08ee8d7f03b2c548bd115ad2a697
oscardhc <oscardhc@sjtu.edu.cn>
2018-12-03 13:59:26 +0800

    Initial Commit

---
+++ ACM_Programming_2018_Homework_4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ACM_Programming_2018_Homework_4
+//
+//  Created by Haichen Dong on 12/3/18.
+//  Copyright © 2018 Haichen Dong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
